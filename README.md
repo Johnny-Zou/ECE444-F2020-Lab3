f2026d0e74b47a8be333409af6e3d1e4d0fab2ee
Johnny-Zou <johnny.zou@mail.utoronto.ca>
2020-10-01 20:41:04 -0400

    modify example 4 to include email field and check that email address is a u of t address

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Jia Yu (Johnny) Zou \
 This repo is a clone of: https://github.com/miguelgrinberg/flasky
 
-Activity 1: \
+Activity 1:
+
 ![Screenshot Activity 1](./Screenshots/Activity1.PNG?raw=true "Activity 1")
+
+Activity 2:
+
+![Screenshot Activity 2_a](./Screenshots/Activity2_a.PNG?raw=true "Activity 2_a")
+---
+![Screenshot Activity 2_b](./Screenshots/Activity2_b.PNG?raw=true "Activity 2_b")
+---
+![Screenshot Activity 2_c](./Screenshots/Activity2_c.PNG?raw=true "Activity 2_c")
+---
+![Screenshot Activity 2_d](./Screenshots/Activity2_d.PNG?raw=true "Activity 2_d")
--- hello.py
+++ hello.py
@@ -1,21 +1,44 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Regexp
+import re
+
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	email = StringField('What is your UofT Email address?', validators=[DataRequired(),Email(message="Please enter a valid email address")])
+	submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html',current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+        	flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        if re.search("utoronto",form.email.data):
+        	session['UofTEmail'] = True
+        else:
+        	session['UofTEmail'] = False
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), valid_email=session.get('UofTEmail'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
-
-
 
 # error code routes
 @app.errorhandler(404)
